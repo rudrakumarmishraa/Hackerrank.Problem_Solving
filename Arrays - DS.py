de5c8b21bf70b328adfe4b444322e563be43cdfd
Rudra Kumar Mishra <137492251+rudrakumarmishraa@users.noreply.github.com>
2023-11-01 20:38:54 +0530

    Arrays - DS

---
+++ Arrays - DS.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+import os
+
+def reverseArray(a):
+   new_lst = a[::-1]
+   return new_lst
+ 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
